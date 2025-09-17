ee93a1a6b50653cad655eae2b25391560e6898ef
garonazarian <garonazarian09@gmail.com>
2025-09-17 21:08:57 +0700

    - update back to previous, seems like flex.global owns flex living?

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export function RootLayout() {
   return (
     <div className="flex flex-col h-screen">
-      <nav className="flex items-center px-16 py-10 bg-argile-100 text-gray-800 shadow-md">
+      <nav className="flex items-center px-16 py-8 bg-argile-100 text-gray-800 shadow-md">
         <div className="flex items-center gap-10">
           {' '}
           {/* Increased gap for spacing */}
@@ -14,6 +14,11 @@ export function RootLayout() {
               alt="Flex Living Logo"
               className="h-6 w-auto"
             />
+            {/*<img*/}
+            {/*  src="https://theflex.global/_next/image?url=https%3A%2F%2Flsmvmmgkpbyqhthzdexc.supabase.co%2Fstorage%2Fv1%2Fobject%2Fpublic%2Fwebsite%2FUploads%2FGreen_V3%2520Symbol%2520%26%2520Wordmark%2520(1).png&w=256&q=75"*/}
+            {/*  alt="Flexliving Logo"*/}
+            {/*  className="h-10 w-fit mb-5"*/}
+            {/*/>*/}
           </Link>
           <div className="flex items-center gap-6 text-sm">
             <Link
