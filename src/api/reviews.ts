9486459ba435565052c84b176cc221447741da57
garonazarian <garonazarian09@gmail.com>
2025-09-18 17:39:00 +0700

    added prod env var

--- src/api/reviews.ts
+++ src/api/reviews.ts
@@ -2,32 +2,28 @@ import { useQuery, useQueryClient, useMutation } from '@tanstack/react-query';
 import { reviewsSchema, type Review } from '../schemas';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || '';
-const ACCOUNT_ID = import.meta.env.VITE_HOSTAWAY_ACCOUNT_ID;
-const API_KEY = import.meta.env.VITE_HOSTAWAY_API_KEY;
-
-if (!API_BASE_URL) {
-  throw new Error(
-    'VITE_API_BASE_URL is not defined. Please check your .env file.',
-  );
-}
+const isProduction = import.meta.env.PROD;
 
 /**
  * Fetches the reviews from our mock API, then parses the response
  * against our Zod schema to ensure it's type-safe.
  */
 const getReviews = async (): Promise<Review[]> => {
-  // TODO: Important: You must implement the API route that fetches and normalizes
-  //  reviews (e.g. GET /api/reviews/hostaway). This route will be tested and
-  //  should return structured, usable data for the frontend.
-  // Although our mock API doesn't need these, this is how you would
-  // structure the call for a real-world, authenticated API.
+  // Determine the correct API endpoint based on the environment
+  const endpoint = isProduction ? `/api/reviews` : `${API_BASE_URL}/reviews`;
+
   const requestHeaders = new Headers();
-  if (ACCOUNT_ID && API_KEY) {
-    requestHeaders.set('X-Account-ID', ACCOUNT_ID);
-    requestHeaders.set('Authorization', `Bearer ${API_KEY}`);
+  if (isProduction) {
+    // These headers are only needed for the Vercel function
+    const ACCOUNT_ID = import.meta.env.VITE_HOSTAWAY_ACCOUNT_ID;
+    const API_KEY = import.meta.env.VITE_HOSTAWAY_API_KEY;
+    if (ACCOUNT_ID && API_KEY) {
+      requestHeaders.set('X-Account-ID', ACCOUNT_ID);
+      requestHeaders.set('Authorization', `Bearer ${API_KEY}`);
+    }
   }
 
-  const response = await fetch(`/api/reviews/hostaway`, {
+  const response = await fetch(endpoint, {
     headers: requestHeaders,
   });
 
@@ -36,9 +32,6 @@ const getReviews = async (): Promise<Review[]> => {
   }
   const data = await response.json();
 
-  // Use the Zod schema to parse and validate the data.
-  // This will throw an error if the API response doesn't match the schema.
-  // TODO: TASK 2: Parse and normalize reviews by listing, review type, channel, and date. (DONE)
   return reviewsSchema.parse(data);
 };
 
